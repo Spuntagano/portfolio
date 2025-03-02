0fb04c0e15f18681382f5f0c81f82f87e5157995
Pierre-Luc Gagné <pierreluc212@gmail.com>
2025-03-02 15:24:03 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -113,7 +113,7 @@ export default function App() {
         <a href="mailto:pierreluc212@gmail.com" title={language === 'fr' ? 'Courriel' : 'Email'}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#b4b4b4" d="M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256l0 32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32l0 80 0 32c0 17.7 14.3 32 32 32s32-14.3 32-32l0-32c0-106-86-192-192-192zm64 192a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"/></svg>
         </a>
-        <a href={language === 'fr' ? '/resume-fr.pdf' : '/resume-en.pdf'} target="_blank" title={language === 'fr' ? 'CV' : 'Resume'}>
+        <a href={language === 'fr' ? 'resume-fr.pdf' : 'resume-en.pdf'} target="_blank" title={language === 'fr' ? 'CV' : 'Resume'}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#b4b4b4" d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"/></svg>
         </a>
       </div>
